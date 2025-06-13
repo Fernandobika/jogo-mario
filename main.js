cad3b5363086d737527eaf9a38da41915e52931c
Fernandobika <alves.rodrigues.fernando@escola.pr.gov.br>
2025-06-13 10:22:01 -0300

    .

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const mario=document.querySelector(".mario")
 const pipe=document.querySelector(".pipe")
 
-const jump= - () => {
+const jump= () => {
     mario.classList.add("jump");
 
     setTimeout(() => {
